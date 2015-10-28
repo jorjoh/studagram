86412c512014d41daf08877fd1c1b01f3881d720
Jørgen Johansen <post@jorgenjohansen.no>
2015-10-28 12:29:06 +0100

    La til testProsjekt

---
+++ testProsjekt/testProsjekt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testProsjekt.Startup))]
+namespace testProsjekt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
