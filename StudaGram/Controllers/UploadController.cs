7534ef78f01ee97f21338ab4d1a0bf4c66ebeb90
Espen Moe <espen357moe@gmail.com>
2015-11-03 21:58:59 +0100

    Upload fungerer nå, men lagrer foreløpig filen til App Data-mappa i stedet for i en blob

---
+++ StudaGram/Controllers/UploadController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StudaGram.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            //file.SaveAs(Server.MapPath("~/App_Data/Test.jpg"));
+            Request.Files["Upload"].SaveAs(Server.MapPath("~/App_Data/Test.jpg"));
+            return View();
+        }
+    }
+}
