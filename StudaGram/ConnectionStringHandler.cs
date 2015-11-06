b40338a490f6cbc9c81d9e0159a84d44e0fa67b9
Espen Moe <espen357moe@gmail.com>
2015-11-06 14:06:21 +0100

    Lagd skjelett til klasse som henter connection strings fra environment variables, og så etterhvert skal skrive dem til config-fila

---
+++ StudaGram/ConnectionStringHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudaGram
+{
+    public class ConnectionStringHandler
+    {
+        private string SqlConnectionString { get; set; }     
+        private string AzureStorageConnectionString { get; set; }
+
+        public void retrieveConnectionStrings()
+        {
+            SqlConnectionString = System.Environment.GetEnvironmentVariable("SQLAZURECONNSTR");
+            if(SqlConnectionString == null)
+            {
+                SqlConnectionString = "UseDevelopmentStorage";
+            }
+
+            AzureStorageConnectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            if(AzureStorageConnectionString == null)
+            {
+                AzureStorageConnectionString = "UseDevelopmentStorage";
+            }
+        }
+    }
+}
